567c6cff772dc3f9679003ff423be2f711319591
MACLIN-TONI <themaclintoni1706@gmail.com>
2026-09-15 07:45:37 +0000

    designed asynchronous 0 to 7  up counter using posedge D flip flops

---
+++ digital_circuits/sequential_circuits/counter/asynchronous_counters/up_counter/up_posedge/tb/tb_up_posedge.v
@@ -0,0 +1,47 @@
+`timescale 1ns/1ps
+
+module tb_up_posedge;
+    reg clk;
+    reg en;
+    reg areset_n;
+    wire [2:0] out;
+
+    up_posedge dut(
+        .clk(clk),
+        .en(en),
+        .areset_n(areset_n),
+        .out(out)
+    );
+
+    initial begin
+        $dumpfile("waves/dump.vcd");
+        $dumpvars(0,tb_up_posedge);
+    end
+
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+
+    initial begin
+        en = 0;
+        areset_n = 0;
+    end
+
+    task check;
+        begin
+            $display("en = %b | areset_n = %b | out = %b",en,areset_n,out);
+        end
+    endtask
+
+    initial begin
+        #10 areset_n = 1;
+        #7 en = 1;
+
+        repeat(20) begin
+            @(negedge clk) check();
+        end
+
+        #10 $finish; 
+    end
+endmodule
