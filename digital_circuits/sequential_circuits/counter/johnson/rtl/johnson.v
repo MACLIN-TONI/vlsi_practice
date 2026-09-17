3f4a57604a430f52f18f00f4b0dc26363c9a4596
MACLIN-TONI <themaclintoni1706@gmail.com>
2026-09-17 06:38:30 +0000

    designed 4 bit Johnson counter

---
+++ digital_circuits/sequential_circuits/counter/johnson/rtl/johnson.v
@@ -0,0 +1,54 @@
+module johnson (
+    input clk,
+    input en,
+    input areset_n,
+    input load_shift,
+    output [3:0] out
+);
+
+    wire [3:0] w , in;
+    assign in = 4'b0000;
+    assign out = w;
+
+    mux_D u3(
+        .clk(clk),
+        .en(en),
+        .areset_n(areset_n),
+        .p_in(in[3]),
+        .s_in(~w[0]),
+        .load_shift(load_shift),
+        .out(w[3])
+    );
+
+    mux_D u2(
+        .clk(clk),
+        .en(en),
+        .areset_n(areset_n),
+        .p_in(in[2]),
+        .s_in(w[3]),
+        .load_shift(load_shift),
+        .out(w[2])
+    );
+
+    mux_D u1(
+        .clk(clk),
+        .en(en),
+        .areset_n(areset_n),
+        .p_in(in[1]),
+        .s_in(w[2]),
+        .load_shift(load_shift),
+        .out(w[1])
+    );
+
+    mux_D u0(
+        .clk(clk),
+        .en(en),
+        .areset_n(areset_n),
+        .p_in(in[0]),
+        .s_in(w[1]),
+        .load_shift(load_shift),
+        .out(w[0])
+    );
+
+    
+endmodule 
---
+++ digital_circuits/sequential_circuits/counter/johnson/tb/tb_johnson.v
@@ -0,0 +1,51 @@
+`timescale 1ns/1ps
+
+module tb_johnson;
+
+    reg clk;
+    reg en;
+    reg areset_n;
+    reg load_shift;
+    wire [3:0] out;
+
+    johnson dut (
+        .clk(clk),
+        .en(en),
+        .areset_n(areset_n),
+        .load_shift(load_shift),
+        .out(out)
+    );
+
+    initial begin
+        $dumpfile("waves/dump.vcd");
+        $dumpvars(0,tb_johnson);
+
+    end
+
+    initial begin
+        clk = 0;
+        en=0;
+        areset_n =0;
+        load_shift = 0;
+        forever #5 clk = ~clk;
+    end
+
+    task check;
+        begin
+            $display("en = %b | areset_n = %b | out = %b",en,areset_n,out);
+        end
+    endtask
+
+    initial begin
+        #7 areset_n = 1;
+        #7 en = 1;
+        #7 load_shift=1;
+        #7 load_shift = 0;
+        #7;
+        repeat (10) begin
+            @(negedge clk) check();
+        end 
+
+        #10 $finish;
+    end
+endmodule
