67fe14b509ad63f99c684dd17941bf3f06bc72ef
MACLIN-TONI <themaclintoni1706@gmail.com>
2026-09-17 08:11:47 +0000

    designed 1010 sequence detector using non overlapping mealy fsm

---
+++ digital_circuits/sequential_circuits/fsm/sequence_detector/s_1010/mealy/s_1010_non_overlapping/rtl/s_1010_non_overlapping.v
@@ -0,0 +1,84 @@
+module s_1010_non_overlapping (
+    input clk,
+    input en,
+    input areset_n,
+    input in,
+    output reg out
+);
+    parameter a=2'b00;
+    parameter b=2'b01;
+    parameter c=2'b10;
+    parameter d=2'b11;
+
+    reg [1:0] state, next;
+
+    always @(posedge clk, negedge areset_n) begin
+        if(~areset_n) begin
+            state <= a;
+        end
+        else begin
+            if(en) begin
+                state <= next;
+            end
+            else begin
+                state <= state;
+            end
+        end
+    end
+
+    always @(state,in) begin
+        next = a;
+        out = 0;
+
+        case (state) 
+            a: begin
+                if(in) begin
+                    next = b;
+                    out = 0;
+                end
+                else begin
+                    next = a;
+                    out = 0;
+                end
+            end
+
+            b: begin
+                if(in) begin
+                    next = b;
+                    out = 0;
+                end
+                else begin
+                    next = c;
+                    out = 0;
+                end
+            end
+
+            c: begin
+                if(in) begin
+                    next = d;
+                    out = 0;
+                end
+                else begin
+                    next = a;
+                    out = 0;
+                end
+            end
+
+            d: begin
+                if(in) begin
+                    next = b;
+                    out = 0;
+                end
+                else begin
+                    next = a;
+                    out = 1;
+                end
+            end
+
+            default: begin
+                next = a;
+                out = 0;
+            end
+        endcase
+    end
+endmodule
